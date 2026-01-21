0fd89477184eb26a501fdbde6c34bdc9d3d54d78
asdfsdfdsfsd <asdfsdfsa@example.com>
2026-01-20 19:06:26 -0800

    a

--- js/02-word-translator.js
+++ js/02-word-translator.js
@@ -1,12 +1,12 @@
 let prefLang = prompt('Which language do you prefer? Type "es" for español, "de" for deutsch, "en" for english, and "fr" for française')
 
-if (prefLang = "es") {
+if (prefLang === "es") {
     document.write('Hola Mundo')
-} else if (prefLang = "de") {
+} else if (prefLang === "de") {
     document.write('Hallo Welt')
-} else if (prefLang = "en") {
+} else if (prefLang === "en") {
     document.write('Hello World')
-} else if (prefLang = "fr") {
+} else if (prefLang === "fr") {
     document.write('Bonjour le Monde')
 }
 else {
